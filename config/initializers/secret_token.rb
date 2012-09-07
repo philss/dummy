22287e44bbc6cd79815ebdb16cfcbbed5711ef61
Philip Sampaio <philip.sampaio@gmail.com>
2012-09-07 02:07:47 -0300

    Dummy with zurb-foundation'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'c54a384a49ab1fe5def8f15bba252e8609114872cce9e037df34b048f7a02f16ba30c1b65353cb8a0dcd0b6b5008ee89b422854fa3eb2f7e2b75519e1c30ae82'
