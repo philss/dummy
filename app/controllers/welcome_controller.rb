3de884f5da98a53c0b0647a0cc371672a450c558
Philip Sampaio <philip.sampaio@gmail.com>
2012-09-09 22:38:06 -0300

    Dummy devise

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  before_filter :authenticate_user!
+  def index
+    @user = current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Dummy::Application.routes.draw do
+  devise_for :users
+
+  get "welcome/index"
+  root :to => 'welcome#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Dummy::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
